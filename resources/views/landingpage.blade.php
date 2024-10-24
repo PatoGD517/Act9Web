2c4cc7771c2f1b428b47999d6aa771303eaee12d
PatoGD517 <patoguerra517@gmail.com>
2024-10-23 18:32:56 -0600

    Initial commit, login dashboard with auth

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Landing Page</title>
+</head>
+<body>
+    <h1>Welcome to the Landing Page</h1>
+    <p>This is a landing page for unregistered users.</p>
+    <a href="{{ route('login') }}">Login</a>
+    <a href="{{ route('register') }}">Register</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Auth::check() ? view('dashboard') : view('landingpage');
 });
+
+// Name the dashboard route so it can be referenced
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard'); // Adding the name
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
